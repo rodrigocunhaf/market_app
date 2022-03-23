cd04950ac450ecd377e8b6a04dda70317b79280e
Rodrigo Cunha França <rodrigocunhaf@github.com>
2022-03-23 14:32:01 -0300

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const server = require('./configuration/server/server.js');
 const express = require('express');
 const userRoutes = require('./modules/user/routes.js');
+const cors = require('cors');
 
 server.use(express.urlencoded({extended:true}))
 server.use(express.json());
+server.use(cors());
 
 server.use(userRoutes);
