54ade60982082e11061f32c6b42351408f910b58
Rodrigo Cunha França <rodrigocunhaf@github.com>
2022-03-23 03:34:11 -0300

    teste live

--- src/configuration/db/connectionDatabase.js
+++ src/configuration/db/connectionDatabase.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 const connectionDatabase = async () => { 
-    await mongoose.connect('mongodb://localhost:27017/market_app');
+    await mongoose.connect('mongodb+srv://rodrigo-admin:j6XqbEtCA5HD27Jl@cluster0.2plds.mongodb.net/market-app');
 }
+
 
 module.exports = connectionDatabase;
